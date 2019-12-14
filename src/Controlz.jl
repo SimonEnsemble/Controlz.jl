470c19511d8e5443add5ec52b55efc896565e09f
SimonEnsemble <cory.simon@oregonstate.edu>
2019-12-14 12:31:50 -0800

    main module

--- src/Controlz.jl
+++ src/Controlz.jl
@@ -5,10 +5,15 @@ using DifferentialEquations
 
 include("tf.jl")
 include("show.jl")
+include("sim.jl")
+include("input_zoo.jl")
 
 export
     # tf.jl
-    TransferFunction, zeros_poles_gain, evaluate
+    TransferFunction, zeros_poles_gain, evaluate, s, proper, strictly_proper,
     # show.jl
-
+    # sim.jl
+    simulate,
+    # input_zoo.jl
+    unit_step
 end
