022c29221b4468b4658d8460bd9f40aa4b391563
SimonEnsemble <cory.simon@oregonstate.edu>
2019-12-20 21:14:06 -0800

    deply docs for gihub pages

--- docs/make.jl
+++ docs/make.jl
@@ -13,11 +13,11 @@ makedocs(
     format = Documenter.HTML(assets = ["assets/flux.css"])
 )
 
- # deploydocs(
- #     repo = "github.com/SimonEnsemble/Controlz.jl.git",
- #     # This is a link to the main repo and the master branch
- #     # target = "build",
- #     julia = "1.3",
- #     osname = "linux",
- #     deps = Deps.pip("mkdocs", "mkdocs-material", "pymdown-extensions") # These are dependencies for the site, not the package
- # )
+deploydocs(
+    repo = "github.com/SimonEnsemble/Controlz.jl.git",
+    # This is a link to the main repo and the master branch
+    # target = "build",
+    julia = "1.3",
+    osname = "linux",
+    deps = Deps.pip("mkdocs", "mkdocs-material", "pymdown-extensions") # These are dependencies for the site, not the package
+)
