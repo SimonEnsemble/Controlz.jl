233c512452fc62e5c711a55d7b9a7cb761ca8953
SimonEnsemble <cory.simon@oregonstate.edu>
2020-02-12 16:32:09 -0800

    docs build probs

--- docs/make.jl
+++ docs/make.jl
@@ -20,5 +20,6 @@ deploydocs(
     repo = "github.com/SimonEnsemble/Controlz.jl.git",
     # This is a link to the main repo and the master branch
     # target = "build",
-    deps = Deps.pip("mkdocs", "mkdocs-material", "pymdown-extensions") # These are dependencies for the site, not the package
+    # versions sugggested by https://discourse.julialang.org/t/mkdocs-material-in-documenter/13764
+    deps = Deps.pip("mkdocs==0.17.5", "mkdocs-material==2.9.4", "pymdown-extensions") # These are dependencies for the site, not the package
 )
