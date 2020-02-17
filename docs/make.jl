2b02f067c4a6806f6c67cb453e2e86b3a1f549f7
CorySimon <CorySimon@berkeley.edu>
2020-02-16 21:23:35 -0800

    thanks docs.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,3 @@
-using Pkg;
-Pkg.activate(joinpath(@__DIR__, "..")); Pkg.instantiate()
-Pkg.activate(); Pkg.instantiate()
-
-pushfirst!(LOAD_PATH, joinpath(@__DIR__, ".."))
-# ^ above from Flux.jl to get this to work
-
 using Documenter
 using Controlz
 
@@ -19,13 +12,13 @@ makedocs(
              "Simulation" => "sim.md",
              "Visualization" => "viz.md",
              "Control systems" => "controls.md",
-             "FAQ" => "faq.md"]
- #     format = Documenter.HTML(assets = ["assets/flux.css"])
+             "FAQ" => "faq.md"],
+    format = Documenter.HTML(assets = ["assets/flux.css"])
 )
 
-deploydocs(
-    repo = "github.com/SimonEnsemble/Controlz.jl.git",
-    # This is a link to the main repo and the master branch
-    # target = "build",
-    deps = Deps.pip("mkdocs", "mkdocs-bootswatch", "pymdown-extensions") # These are dependencies for the site, not the package
-)
+ # deploydocs(
+ #     repo = "github.com/SimonEnsemble/Controlz.jl.git",
+ #     # This is a link to the main repo and the master branch
+ #     # target = "build",
+ #     deps = Deps.pip("mkdocs", "mkdocs-bootswatch", "pymdown-extensions") # These are dependencies for the site, not the package
+ # )
